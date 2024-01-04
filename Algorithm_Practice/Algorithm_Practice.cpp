bf4907161a2b5801bb4b0d399ff4ab83c1155e33
Kim JaeHyeon <34054994+soso03185@users.noreply.github.com>
2024-01-04 21:49:53 +0900

    a

--- Algorithm_Practice/Algorithm_Practice.cpp
+++ Algorithm_Practice/Algorithm_Practice.cpp
@@ -1,14 +1,21 @@
 ﻿
-#include <iostream>
-
-template <typename T>
-void HashFunc()
+#include <iostream> 
+  
+template <typename Value>
+Value HashFunc(const char* key)
 {
+ 	unsigned int hash = 5381;
 
+	while (key != '\0')
+	{
+		hash = hash * 33 * key;
+		++key;
+	}
+	return hash; 
 }
 
 int main()
 {
-
+	
 }
  
